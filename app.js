ee2dd75aea1af148a2c3d54d19936733b18ca5d6
jake <jake@uhuu.com>
2017-02-19 12:18:06 +0100

    2

---
+++ app.js
@@ -0,0 +1,13 @@
+(function () {
+'use strict';
+
+angular.module('myFirstApp', [])
+
+.controller('MyFirstController', function ($scope) {
+  $scope.name = "Jacob";
+  $scope.sayHello = function () {
+    return "hello jyngst!";
+  }
+});
+
+})();
